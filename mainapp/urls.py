f18f914e699c967f1a752d7bba273da0b903262e
polina <polina@Ubuntu-Linux.myguest.virtualbox.org>
2023-07-18 21:08:07 +0300

    Done lesson 5

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,18 +1,3 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from mainapp import views
 from mainapp.apps import MainappConfig
@@ -20,10 +5,11 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', views.MainPageView.as_view()),
-    path("news/", views.NewsPageView.as_view()),
-    path('courses/', views.CoursesPageView.as_view()),
-    path('contacts/', views.ContactsPageView.as_view()),
-    path('doc_site/', views.DocSitePageView.as_view()),
-    path('login/', views.LoginPageView.as_view()),
-]
+    path("", views.MainPageView.as_view(), name="main_page"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path("courses/<int:pk>/", views.CoursesDetailView.as_view(), name="courses_detail"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    ]
